f8493432454d17a4113e4a3172ccebb6c3e9d700
dkks112313 <ovcharenkoden05@gmail.com>
2025-01-02 19:42:06 +0200

    fix FigureSupplier

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,9 +1,19 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class FigureSupplier implements FigureMethods {
     public Figure getRandomFigure() {
-
+        return switch(new Random().nextInt(5)) {
+            case 0 -> new Rectangle(Colors.values()[new Random().nextInt(4)], 10, 10);
+            case 1 -> new Circle(Colors.values()[new Random().nextInt(4)], 10);
+            case 2 -> new Square(Colors.values()[new Random().nextInt(4)], 10);
+            case 3 -> new IsoscelesTrapezoid(Colors.values()[new Random().nextInt(4)], 10, 10, 10);
+            case 4 -> new RightTriangle(Colors.values()[new Random().nextInt(4)], 10, 10);
+            default -> throw new IllegalStateException("Unexpected value: " + new Random().nextInt(5));
+        };
     }
+
     public Figure getDefaultFigure() {
 
     }
@@ -11,11 +21,6 @@ public class FigureSupplier implements FigureMethods {
     @Override
     public double area() {
         return 0;
-    }
-
-    @Override
-    public String color() {
-        return "";
     }
 
     @Override
